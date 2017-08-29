d9c846906f15978a5b12d647cba27fe14f6b221a
ahmadbasir <akhmadbasir5@gmail.com>
2017-08-29 08:20:47 +0700

    securing mail.php

--- config/mail.php
+++ config/mail.php
@@ -1,39 +1,15 @@
 <?php
 
-// return [
-//     'driver' => env('MAIL_DRIVER', 'smtp'),
-//     'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-//     'port' => env('MAIL_PORT', 587),
-//     'from' => [
-//         'address' => env('MAIL_FROM_ADDRESS', 'oauth-05f9f4@inbox.mailtrap.io'),
-//         'name' => env('MAIL_FROM_NAME', 'oauth'),
-//     ],
-//
-//     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-//     'username' => env('MAIL_USERNAME'),
-//     'password' => env('MAIL_PASSWORD'),
-//     'sendmail' => '/usr/sbin/sendmail -bs',
-//     'pretend' => false,
-//     'markdown' => [
-//         'theme' => 'default',
-//
-//         'paths' => [
-//             resource_path('views/vendor/mail'),
-//         ],
-//     ],
-//
-// ];
-
 return array(
   "driver" => "smtp",
   "host" => "smtp.mailgun.org",
-  "port" => 587,
+  "port" => env('MAIL_PORT'),
   "from" => array(
       "address" => "No-Reply_OAuth@mail.com",
       "name" => "OAuth-RESET_PASSWORD"
   ),
-  "username" => "postmaster@sandboxf1d2718da8474ae19bf338a7941b8585.mailgun.org",
-  "password" => "jhagbuyetbiwqu3t728",
+  'username' => env('MAIL_USERNAME'),
+  'password' => env('MAIL_PASSWORD'),
   "sendmail" => "/usr/sbin/sendmail -bs",
   "pretend" => false
 );
